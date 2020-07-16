feb2c875475a5007cac6071be37d7cd3f7332ae0
tony <x2040052@126.com>
2020-07-17 00:44:20 +0800

    using LongAdder instead of AtomicLong

--- src/main/java/io/github/tonybro233/sillybatch/job/BatchMetrics.java
+++ src/main/java/io/github/tonybro233/sillybatch/job/BatchMetrics.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
 
 /**
  * Metrics of batch job.
@@ -17,22 +17,22 @@ public class BatchMetrics implements Serializable {
 
     private volatile Long total;
 
-    private AtomicLong readCount = new AtomicLong();
+    private LongAdder readCount = new LongAdder();
 
-    private AtomicLong processCount = new AtomicLong();
+    private LongAdder processCount = new LongAdder();
 
-    private AtomicLong filterCount = new AtomicLong();
+    private LongAdder filterCount = new LongAdder();
 
-    private AtomicLong writeCount = new AtomicLong();
+    private LongAdder writeCount = new LongAdder();
 
-    private AtomicLong errorCount = new AtomicLong();
+    private LongAdder errorCount = new LongAdder();
 
     public String report() {
-        String str = "{Read: " + readCount.get()
-                + ", Processed: " + processCount.get()
-                + ", Written: " + writeCount.get()
-                + ", Filtered: " + filterCount.get()
-                + ", Failed: " + errorCount.get();
+        String str = "{Read: " + readCount.sum()
+                + ", Processed: " + processCount.sum()
+                + ", Written: " + writeCount.sum()
+                + ", Filtered: " + filterCount.sum()
+                + ", Failed: " + errorCount.sum();
         if (null != total) {
             str += ", total: " + total;
         }
@@ -47,73 +47,73 @@ public class BatchMetrics implements Serializable {
         if (null != startTime && null != endTime) {
             str += ", duration=" + Duration.between(startTime, endTime);
         }
-        str += ", readCount=" + readCount.get()
-                + ", processCount=" + processCount.get()
-                + ", writeCount=" + writeCount.get()
-                + ", filterCount=" + filterCount.get()
-                + ", errorCount=" + errorCount.get()
+        str += ", readCount=" + readCount.sum()
+                + ", processCount=" + processCount.sum()
+                + ", writeCount=" + writeCount.sum()
+                + ", filterCount=" + filterCount.sum()
+                + ", errorCount=" + errorCount.sum()
                 + '}';
         return str;
     }
 
     public long getReadCount() {
-        return readCount.get();
+        return readCount.sum();
     }
 
     public long getProcessCount() {
-        return processCount.get();
+        return processCount.sum();
     }
 
     public long getFilterCount() {
-        return filterCount.get();
+        return filterCount.sum();
     }
 
     public long getWriteCount() {
-        return writeCount.get();
+        return writeCount.sum();
     }
 
     public long getErrorCount() {
-        return errorCount.get();
+        return errorCount.sum();
     }
 
     public void incrementReadCount() {
-        readCount.incrementAndGet();
+        readCount.increment();
     }
 
     public void incrementProcessCount() {
-        processCount.incrementAndGet();
+        processCount.increment();
     }
 
     public void incrementFilterCount() {
-        filterCount.incrementAndGet();
+        filterCount.increment();
     }
 
     public void incrementWriteCount() {
-        writeCount.incrementAndGet();
+        writeCount.increment();
     }
 
     public void incrementErrorCount() {
-        errorCount.incrementAndGet();
+        errorCount.increment();
     }
 
     public void addReadCount(long delta) {
-        readCount.addAndGet(delta);
+        readCount.add(delta);
     }
 
     public void addProcessCount(long delta) {
-        processCount.addAndGet(delta);
+        processCount.add(delta);
     }
 
     public void addFilterCount(long delta) {
-        filterCount.addAndGet(delta);
+        filterCount.add(delta);
     }
 
     public void addWriteCount(long delta) {
-        writeCount.addAndGet(delta);
+        writeCount.add(delta);
     }
 
     public void addErrorCount(long delta) {
-        errorCount.addAndGet(delta);
+        errorCount.add(delta);
     }
 
     public LocalDateTime getStartTime() {
